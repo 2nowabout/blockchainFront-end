4bf52127ab38908d36567691bd275862dd3d3807
Duncan Schoenmakers <400156@student.fontys.nl>
2021-12-28 13:54:50 +0100

    worked on userpage and restAPI connector

---
+++ src/restCon/restCon.js
@@ -0,0 +1,50 @@
+async function sendToServer(link, type, data) {
+  // When GET request
+  let response;
+  if (type === requestTypes.GET) {
+    var url = new URL(link);
+    url.search = new URLSearchParams(data).toString();
+    response = await fetch(url.href, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  // When POST, PUT OR DELETE
+  else {
+    var headers = null;
+    headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    };
+    response = await fetch(link, {
+      method: type,
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+  }
+  return response;
+}
+
+/* ------------------------------------------ */
+export const requestTypes = Object.freeze({
+  GET: "GET",
+  POST: "POST",
+});
+
+export async function makeHttpCall(link, type, body) {
+  if (typeof link === "string" || link instanceof String) {
+    var toReturn = null;
+    await sendToServer(link, type, body)
+      .then(function (response) {
+        toReturn = response;
+      })
+      .catch(function (error) {
+        return error;
+      });
+    return toReturn;
+  }
+}
--- src/views/userpage.vue
+++ src/views/userpage.vue
@@ -3,13 +3,22 @@
     <TopBar />
     <v-container>
       <v-row>
-        <v-col v-for="index in 3" v-bind:key="index" cols="6">
+        <v-col cols="6">
           <v-card>
             <v-card-title style="text-align: center"
               >1. Upload a certificate</v-card-title
             >
             <v-card-actions class="justify-center">
-              <v-file-input truncate-length="15"> </v-file-input>
+              <v-file-input
+                truncate-length="15"
+                v-model="uploadedFile"
+                label="Upload certificate"
+              >
+              </v-file-input>
+            </v-card-actions>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="Upload()"> Upload </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -21,6 +30,14 @@
 <script>
 import TopBar from "@/components/TopBar";
 export default {
+  data() {
+    return {
+      uploadedFile: null,
+    };
+  },
+  methods: {
+    Upload() {},
+  },
   components: {
     TopBar: TopBar,
   },
